a272c1bcdfe7c12aa7e51439f68d58cefbf1efb0
Alena <i.am.am.629@gmail.com>
2018-08-03 17:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('nested.target')
+}
+
+function increaseRankBy(n){
+  var l = document.querySelectorAll('ul.ranked-list')
+  for (var i = 0; i < l.length; i++) {
+    l[i].innerHTML = parseInt(l[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  
+}
